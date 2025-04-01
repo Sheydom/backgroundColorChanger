d59d76743a2d9fbb26cecc0184edeb7996036f30
Dom <dominic.knabe@gmail.com>
2025-04-02 10:06:05 +1100

    typo

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ import gulp from "gulp";
 import shell from "gulp-shell";
 import { rimraf } from "rimraf";
 
+// gulp task to clean the dist folder before building
 gulp.task("clean", async () => {
   await rimraf("dist");
 });
-// gulp task to clean the dist folder before building
 
 // gulp task to build the project using Parcel
 gulp.task("build", gulp.series("clean", shell.task("parcel build index.html")));
